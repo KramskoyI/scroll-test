e5152a43489a1fe568979f796ffd6b3e6c3d9a31
KramskoyI <91892171+KramskoyI@users.noreply.github.com>
2021-10-20 14:34:44 +0300

    button scrol up

--- scripts/main.js
+++ scripts/main.js
@@ -33,3 +33,26 @@ fifth. onclick = function(){
     five.scrollIntoView( {block: "start", behavior: "smooth"});
     window.scrollBy(0, -60); 
 };
+
+//button scrol up  
+function trackScroll() {
+    let scrolled = window.pageYOffset;
+    let coords = document.documentElement.clientHeight;
+  
+    if (scrolled > coords) {
+        goTopBtn.classList.add('back_to_top-show');
+    }
+    if (scrolled < coords) {
+        goTopBtn.classList.remove('back_to_top-show');
+    }
+}
+  
+function backToTop() {
+    if (window.pageYOffset > 0) {
+        window.scrollBy(0, -80);
+        setTimeout(backToTop, 0);
+    }
+}
+let goTopBtn = document.querySelector('.back_to_top');
+window.addEventListener('scroll', trackScroll);
+goTopBtn.addEventListener('click', backToTop);
