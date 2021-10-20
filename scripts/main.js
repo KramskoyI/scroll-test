5707b5b26bbe191576e6d0a7ba668e32dc6bad65
KramskoyI <91892171+KramskoyI@users.noreply.github.com>
2021-10-20 11:27:49 +0300

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+const first = document.getElementById('first');
+const second = document.getElementById('second');
+const three = document.getElementById('three');
+const fourth = document.getElementById('fourth');
+const fifth = document.getElementById('fifth');
+
+first. onclick = function(){
+    const one = document.getElementById('one');
+    one.scrollIntoView( {block: "start", behavior: "smooth"});
+};
+
+second. onclick = function(){
+    const two = document.getElementById('two');
+    two.scrollIntoView( {block: "start", behavior: "smooth"});
+};
+
+third. onclick = function(){
+    const three = document.getElementById('three');
+    three.scrollIntoView( {block: "start", behavior: "smooth"});
+};
+
+fourth. onclick = function(){
+    const four = document.getElementById('four');
+    four.scrollIntoView( {block: "start", behavior: "smooth"});
+};
+
+fifth. onclick = function(){
+    const five = document.getElementById('five');
+    five.scrollIntoView( {block: "start", behavior: "smooth"});
+    
+    
+};
