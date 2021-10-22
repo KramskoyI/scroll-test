292abdcbdf890b0e7b82659e816b6f1313dba983
KramskoyI <91892171+KramskoyI@users.noreply.github.com>
2021-10-22 19:01:22 +0300

    fixed error all

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,24 @@
 const linkList = document.getElementById('linkList');
-const first = document.getElementById('first')
+const scrollTop = 300;
+let goTopBtn = document.querySelector('.back_to_top');
+let one = document.getElementById('one');
 
-linkList.addEventListener('click', function(){
-    let data = event.target.getAttribute('data-section');
-    let id = data;
+window.addEventListener('scroll', trackScroll);
+goTopBtn.addEventListener('click', backToTop);
+linkList.addEventListener('click', function(event){
+    let id = event.target.getAttribute('data-section');
     let paragraph = document.getElementById(id);
     paragraph.scrollIntoView( {block: "start", behavior: "smooth"});
-    
 });
 
 //button scrol up  
 function trackScroll() {
-    let scrolled = window.pageYOffset;
-    let coords = document.documentElement.clientHeight;
-    if (scrolled > coords) {
+    if (window.pageYOffset >= scrollTop) {
         goTopBtn.classList.add('back_to_top-show');
-    }
-    if (scrolled < coords) {
-        goTopBtn.classList.remove('back_to_top-show');
+    } else { goTopBtn.classList.remove('back_to_top-show');
     }
 }
-function backToTop() {
-    if (window.pageYOffset > 0) {
-        window.scrollBy(0, -60);
-        setTimeout(backToTop, 0);
-    }
-}
-let goTopBtn = document.querySelector('.back_to_top');
-window.addEventListener('scroll', trackScroll);
-goTopBtn.addEventListener('click', backToTop);
+ function backToTop() {
+    one.scrollIntoView( {block: "start", behavior: "smooth"});
+};
+
